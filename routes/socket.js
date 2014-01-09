c34e427a1b428c9c61b348fec4fc17bd5fc3c82f
Maxim McNair <mail@maximmcnair.com>
2014-01-09 13:42:51 +0000

    Projects are only shared between team members

--- routes/socket.js
+++ routes/socket.js
@@ -50,9 +50,14 @@ var userNames = (function () {
 
 // export function for listening to the socket
 module.exports = function (socket) {
+  // console.log(socket)
   // console.log(socket.namespace.manager.handshaken)
   // console.log(socket.namespace.manager.handshaken[0].headers)
-  // console.log(socket.manager.handshaken[socket.store.id].headers)
+  var team = socket.manager.handshaken[socket.store.id].user.team
+  console.log('team: ', team)
+
+  socket.join(team)
+
   // console.log(socket.store.id)
   var name = userNames.getGuestName();
 
@@ -62,41 +67,9 @@ module.exports = function (socket) {
     users: userNames.get()
   });
 
-  // notify other clients that a new user has joined
-  socket.broadcast.emit('user:join', {
-    name: name
-  });
-
-  // broadcast a user's message to other users
-  socket.on('send:message', function (data) {
-    socket.broadcast.emit('send:message', {
-      user: name,
-      text: data.message
-    });
-  });
-
-  // validate a user's name change, and broadcast it on success
-  socket.on('change:name', function (data, fn) {
-    if (userNames.claim(data.name)) {
-      var oldName = name;
-      userNames.free(oldName);
-
-      name = data.name;
-
-      socket.broadcast.emit('change:name', {
-        oldName: oldName,
-        newName: name
-      });
-
-      fn(true);
-    } else {
-      fn(false);
-    }
-  });
-
   // broadcast a user's project to other users
   socket.on('send:project', function (data) {
-    socket.broadcast.emit('send:project', {
+    socket.broadcast.in(team).emit('send:project', {
       title: data.title,
       description: data.description
     });
