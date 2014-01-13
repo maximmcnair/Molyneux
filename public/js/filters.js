0f44aa0f0530bc12c4dbee5b66398e8d69ef51f1
Maxim McNair <mail@maximmcnair.com>
2014-01-13 19:19:07 +0000

    Create timePretty filter to app

--- public/js/filters.js
+++ public/js/filters.js
@@ -2,9 +2,45 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', function (version) {
+angular.module('myApp.filters', [])
+  .filter('interpolate', function (version) {
     return function (text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  });
+  })
+  .filter('timePretty', function () {
+    return function (text) {
+      var milliseconds = new Date(text)
+
+      // TIP: to find current time in milliseconds, use:
+      // var milliseconds_now = new Date().getTime();
+
+      var seconds = milliseconds / 1000;
+      // var numyears = Math.floor(seconds / 31536000);
+      // if(numyears){
+      //     return numyears + 'year' + ((numyears > 1) ? 's' : '');
+      // }
+      // var numdays = Math.floor((seconds % 31536000) / 86400);
+      // if(numdays){
+      //     return numdays + 'day' + ((numdays > 1) ? 's' : '');
+      // }
+      var numhours = Math.floor(((seconds % 31536000)) / 3600)
+      ,   numMins = Math.round(((((seconds % 31536000)) / 3600) - numhours) * 60);
+      // console.log( (((seconds % 31536000) % 86400) / 3600) - numhours) * 60;
+      if(numhours){
+          var hours = numhours + 'hr' + ((numhours > 1) ? 's' : '')
+          ,   mins = numMins + 'min' + ((numMins > 1) ? 's' : '');
+
+          return hours + ' ' + ((numMins > 1) ? mins : '');
+      }
+      var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+      if(numminutes){
+          return numminutes + 'min' + ((numminutes > 1) ? 's' : '');
+      }
+      var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+      if(numseconds){
+          return numseconds.toFixed() + 'sec' + ((numseconds > 1) ? 's' : '');
+      }
+      return '0sec'; //'just now' //or other string you like;
+    }
+  })
