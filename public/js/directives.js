f2834f953b142cfe0a06f1ddbf3663c187e9a9ff
Maxim McNair <mail@maximmcnair.com>
2014-03-04 17:25:19 +0000

    Added basic CRUD for timers

--- public/js/directives.js
+++ public/js/directives.js
@@ -8,17 +8,47 @@ angular.module('myApp.directives', [])
       elm.text(version)
     }
   })
-  .directive('timer', function (TimerService, $rootScope, $http) {
+  .directive('timer', function (TimerService, ProjectService, $rootScope, $http) {
     return {
       restrict: 'E'
     , scope: true
     , controller: function ($scope, $element) {
         $scope.timePretty = timePretty
 
+        //========================================================
+        //  Editable func
+        //========================================================
         $scope.editable = false
-        // $scope.edit = function () {
-        //   $scope.editable = !$scope.editable
-        // }
+        $scope.tags = []
+        $scope.projects = []
+        $scope.formatLabel = function (model) {
+          for (var i = 0; i < $scope.projects.length; i++) {
+            if ($scope.projects[i] && model === $scope.projects[i]._id){
+              return $scope.projects[i].title
+            }
+          }
+        }
+        $scope.edit = function () {
+          $scope.editable = true
+          $http.get('/api/tags').success(function(data){
+            console.log('tags GET success', data)
+            $scope.tags = data
+          })
+          $scope.projects = ProjectService.get({}, function (res) {
+            console.log(res)
+          })
+        }
+        $scope.save = function () {
+          $http.put('/api/timer/' + $scope.timer._id, $scope.timer).
+            success(function(data, status, headers, config) {
+              console.log('success', data)
+              $rootScope.$broadcast('TimerEdited', data)
+              $scope.editable = false
+            }).
+            error(function(data, status, headers, config) {
+              console.log('error', data)
+            })
+        }
         $scope.continue = function () {
           console.log('continue', $scope.timer._id)
 
@@ -55,7 +85,7 @@ angular.module('myApp.directives', [])
 
         $scope.editable = false
         $scope.edit = function () {
-          $scope.editable = !$scope.editable
+          $scope.editable = true
         }
         $scope.save = function () {
           $http.put('/api/project/' + $scope.project._id, $scope.project).
