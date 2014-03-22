8388728de8a4d5cba2ec300ae820dbade69b1434
Maxim McNair <mail@maximmcnair.com>
2014-03-22 12:47:58 +0000

    time directive updates ng model on increase/decrease

--- public/js/directives/time.js
+++ public/js/directives/time.js
@@ -31,45 +31,56 @@ angular.module('myApp')
     }
   })
   .directive('number', function () {
-    return function ($scope, element, attrs) {
-      element.bind("keydown keypress", function (event) {
-        // console.log('event.which:', event.which)
-        // Only allow numbers to be added
-        if (event.which > 47 && event.which < 58){
-          return event
-        }
-        // up key to increase
-        if (event.which === 38){
-          // Stop number from being above 60
-          if(attrs.minute && element[0].value > 59){
-            return false
+    return {
+      restrict: 'A'
+    , require: '^ngModel'
+    , scope: {
+        ngModel: '='
+      }
+    , link: function ($scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+          // console.log('event.which:', event.which)
+          // Only allow numbers to be added
+          if (event.which > 47 && event.which < 58){
+            return event
           }
-          element[0].value++
-          // Add zero to front of number if below 10
-          if(attrs.minute && element[0].value < 10){
-            element[0].value = '0' + element[0].value
+          // up key to increase
+          if (event.which === 38){
+            // Stop number from being above 60
+            if(attrs.minute && $scope.ngModel > 59){
+              return false
+            }
+            $scope.$apply(function(){
+              $scope.ngModel++
+              // Add zero to front of number if below 10
+              if(attrs.minute && $scope.ngModel < 10){
+                $scope.ngModel = '0' + $scope.ngModel
+              }
+            })
           }
-        }
-        // down key to decrease
-        if (event.which === 40){
-          if(element[0].value > 0){
-            element[0].value--
-            // Add zero to front of number if below 10
-            if(attrs.minute && element[0].value < 10){
-              element[0].value = '0' + element[0].value
+          // down key to decrease
+          if (event.which === 40){
+            if($scope.ngModel > 0){
+              $scope.$apply(function(){
+                $scope.ngModel--
+                // Add zero to front of number if below 10
+                if(attrs.minute && $scope.ngModel < 10){
+                  $scope.ngModel = '0' + $scope.ngModel
+                }
+              })
             }
           }
-        }
-        // return keys
-        if(
-          event.which === 9 || // tab
-          event.which === 8 || // backspace
-          event.which === 37 || // left
-          event.which === 39 // right
-          ){
-          return
-        }
-        return false
-      })
+          // return keys
+          if(
+            event.which === 9 || // tab
+            event.which === 8 || // backspace
+            event.which === 37 || // left
+            event.which === 39 // right
+            ){
+            return
+          }
+          return false
+        })
+      }
     }
   })
